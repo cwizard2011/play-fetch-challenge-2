bca3794dfd26b7dec5fee279bfeff92f41fdf281
Peter Adeoye <cwizard2011@gmail.com>
2022-09-05 22:17:53 +0100

    initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float allowNextPress = 0.8f;
+    private float timeSinceLastDog;
+
+    void Start()
+    {
+        timeSinceLastDog = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeSinceLevelLoad - timeSinceLastDog >= allowNextPress)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastDog = Time.timeSinceLevelLoad;
+        }
+    }
+}
